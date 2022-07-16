11dfbf2726c146a17f94a0872201b839d5d49341
Henrique <henrique.rodrigues7@hotmail.com>
2022-07-16 15:01:45 -0300

    update(login): add loginPayload

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoginPayload } from 'src/app/models/payloads/login.payload';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
   constructor() { }
 
-  ngOnInit() {
+  public loginPayload: LoginPayload = {
+    email: '',
+    password: '',
+  }
+
+  public login(): void {
+    console.log(this.loginPayload);
   }
 
 }
