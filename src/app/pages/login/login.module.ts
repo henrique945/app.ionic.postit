133b0310db9595d31e664de37d553dcd059b21fa
Henrique <henrique.rodrigues7@hotmail.com>
2022-07-17 09:13:46 -0300

    update(login): add loading

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { HalfCircleSpinnerModule } from 'angular-epic-spinners';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    HalfCircleSpinnerModule,
   ],
   declarations: [LoginPage]
 })
-export class LoginPageModule {}
+export class LoginPageModule { }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -15,8 +15,22 @@ export class LoginPage {
     password: '',
   }
 
+  public isLoading: boolean = false;
+
   public login(): void {
+    this.isLoading = true;
     console.log(this.loginPayload);
+  }
+
+  public canLogin(): boolean {
+    const regex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
+
+    const emailIsValid = regex.test(this.loginPayload.email)
+
+    if (emailIsValid && this.loginPayload.password.length >= 6)
+      return true;
+
+    return false;
   }
 
 }
