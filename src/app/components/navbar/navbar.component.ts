2c9020173f1df3cc8e3aaa330f6398bd06c037ad
Henrique <henrique.rodrigues7@hotmail.com>
2022-07-17 14:28:30 -0300

    update(navbar): working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { NavbarEnum } from 'src/app/models/enums/navbar.enum';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(
+    private readonly router: Router,
+  ) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((route: NavigationEnd) => {
+        if (route.url.includes('/feed'))
+          this.currentNavbar = NavbarEnum.FEED;
+        if (route.url.includes('/home'))
+          this.currentNavbar = NavbarEnum.HOME;
+        if (route.url.includes('/profile'))
+          this.currentNavbar = NavbarEnum.PROFILE;
+      });
+  }
 
-  ngOnInit() {}
-
+  public navbarEnum: typeof NavbarEnum = NavbarEnum;
+  public currentNavbar: NavbarEnum = NavbarEnum.HOME;
 }
---
+++ src/app/models/enums/navbar.enum.ts
@@ -0,0 +1,5 @@
+export enum NavbarEnum {
+  FEED = 1,
+  HOME = 2,
+  PROFILE = 3,
+}
