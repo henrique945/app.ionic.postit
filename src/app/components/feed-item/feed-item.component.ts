15c23ba78f127c8577e4f95f6215cf3a922dae7a
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-08-06 14:12:02 -0300

    feat(feed): added integration with api

--- src/app/components/feed-item/feed-item.component.ts
+++ src/app/components/feed-item/feed-item.component.ts
@@ -1,24 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { PostItProxy } from 'src/app/models/proxies/postit.proxy';
+import { Component, Input } from '@angular/core';
+import { FeedPostItProxy } from '../../models/proxies/feed-postit.proxy';
+import { NoteService } from '../../services/note.service';
+import { HelperService } from '../../services/helper.service';
 
 @Component({
   selector: 'app-feed-item',
   templateUrl: './feed-item.component.html',
   styleUrls: ['./feed-item.component.scss'],
 })
-export class FeedItemComponent implements OnInit {
+export class FeedItemComponent {
 
-  constructor() { }
+  constructor(
+    private readonly helper: HelperService,
+    private readonly note: NoteService,
+  ) { }
 
   @Input()
-  public postIt: PostItProxy;
+  public postIt: FeedPostItProxy;
 
-  public isLiked: boolean = false;
+  public isLoading: boolean = false;
 
-  ngOnInit() { }
+  public async setLikeToPostIt(): Promise<void> {
+    this.isLoading = true;
+    const [, errorMessage] = await this.note.setLikeOnPostit(this.postIt);
+    this.isLoading = true;
 
-  public setLikeToPostIt(): void {
-    this.isLiked = !this.isLiked;
+    if (errorMessage)
+      return this.helper.showToast(errorMessage, 5_000);
+
+    this.postIt.hasLiked = !this.postIt.hasLiked;
   }
-
 }
--- src/app/models/proxies/comment.proxy.ts
+++ src/app/models/proxies/comment.proxy.ts
@@ -1,3 +1,3 @@
 export interface CommentProxy {
-    comment: string;
+  comment: string;
 }
