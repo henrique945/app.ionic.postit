64e38771026eaa5668b75f2688c217d70b67ba12
MJacob <matheusjb34@gmail.com>
2022-08-07 10:09:09 -0300

    feat(*): heroku requires files to deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const cacheControl = require('express-cache-controller');
+const path = require('path');
+const app = express();
+
+app.use(cacheControl({
+    maxAge: 300,
+}));
+app.use(helmet());
+app.use(compression());
+app.use(express.static(${__dirname}/www));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(${__dirname}/www/index.html));
+});
+
+app.listen(process.env.PORT || 8080);
